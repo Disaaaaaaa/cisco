9c40e1ace7d244e14a04c314ae7de3315b6f4683
Disaaaaaaa <dias.asylbek03@gmail.com>
2026-05-05 10:32:03 +0500

    Fix Vercel output directory error

--- build.js
+++ build.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexPath = path.join(__dirname, 'index.html');
-let html = fs.readFileSync(indexPath, 'utf8');
+const distPath = path.join(__dirname, 'dist');
+if (!fs.existsSync(distPath)) {
+  fs.mkdirSync(distPath);
+}
+
+const sourcePath = path.join(__dirname, 'index.html');
+let html = fs.readFileSync(sourcePath, 'utf8');
 
 const url = process.env.SUPABASE_URL || "";
 const key = process.env.SUPABASE_KEY || "";
@@ -14,5 +19,5 @@ if (!url || !key) {
 html = html.replace('REPLACE_ME_SUPABASE_URL', url);
 html = html.replace('REPLACE_ME_SUPABASE_KEY', key);
 
-fs.writeFileSync(indexPath, html);
-console.log("Successfully injected environment variables into index.html");
+fs.writeFileSync(path.join(distPath, 'index.html'), html);
+console.log("Successfully built project into 'dist' folder");
